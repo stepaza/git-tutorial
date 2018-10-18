913deb457cb1b8a68cfca629f2239a7d046b1b91
Stefan Zahnd <stefanpatrick.zahnd@insel.ch>
2018-10-18 16:09:53 +0200

    Changed hello_world file to investigate branch merge behaviour

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,6 @@
 
 
-print('this is the initial script version')
+print('this is NOT the initial script version - it is an alteration of the first script to evaluate merging behaviour of git')
 
 print('\nHello initial world!')
 
